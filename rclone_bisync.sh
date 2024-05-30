63f8daa99834fd64ce3fb1fe8d50c13be8c27221
Erik-Jan van de Wal <erikfourofour@proton.me>
2024-05-30 13:33:22 -0600

    .gitignore + refactor

--- rclone_bisync.sh
+++ rclone_bisync.sh
@@ -9,6 +9,17 @@ ERROR_FILE="/tmp/rclone_bisync_error.log"
 LOCAL_PATH=""
 REMOTE_PATH=""
 
+# Function to delete logs
+delete_logs() {
+    rm -f "$LOGFILE" "$ERROR_FILE" "$DRYRUN_OUTPUT"
+}
+
+# Check for rclone command
+if ! command -v rclone &> /dev/null; then
+    echo "Error: rclone is not installed or not in PATH."
+    exit 1
+fi
+
 # Parse command-line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -23,7 +34,7 @@ while [[ $# -gt 0 ]]; do
             shift # past argument
             shift # past value
             ;;
-        *)  # unknown option
+        *)
             echo "Unknown option: $key"
             exit 1
             ;;
@@ -42,61 +53,53 @@ if [ -z "$REMOTE_PATH" ]; then
     exit 1
 fi
 
-# Delete logs
-delete_logs() {
-	rm -f "$LOGFILE"
-	rm -f "$ERROR_FILE"
-	rm -f "$DRYRUN_OUTPUT"
-}
-
 # Check if the lock file exists and exit
 if [ -e "$LOCKFILE" ]; then
+    echo "Error: Another instance of the script is already running."
     exit 1
 else
-	# Delete the logs from the previous sync
-	delete_logs
+    # Delete the logs from the previous sync
+    delete_logs
     # Create the lock file
     touch "$LOCKFILE"
-	# Boolean for resync
-	resync=false
+    # Boolean for resync
+    resync=false
     # Run the rclone bisync command with dry-run and capture output
     rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --dry-run 2>&1 | tee "$DRYRUN_OUTPUT"
-	# Rclone might fail with a recoverable error, this happens when there was never an initial sync, or when the paths have no content (possible rclone bug perhaps?)
-	if grep -q "Bisync aborted. Must run --resync to recover" "$DRYRUN_OUTPUT"; then
-		echo "RECOVERING WIT RESYNC FLAG"
-	    # Run the rclone bisync command with dry-run and --resync and capture the output
-		rm -f "$DRYRUN_OUTPUT"
-		resync=true
-    	rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync --dry-run 2>&1 | tee "$DRYRUN_OUTPUT"
-	fi
-	
+    # Rclone might fail with a recoverable error, this happens when there was never an initial sync, or when the paths have no content (possible rclone bug perhaps?)
+    if grep -q "Bisync aborted. Must run --resync to recover" "$DRYRUN_OUTPUT"; then
+        # Run the rclone bisync command with dry-run and --resync and capture the output
+        echo "Recovering with --resync flag"
+        rm -f "$DRYRUN_OUTPUT"
+        resync=true
+        rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync --dry-run 2>&1 | tee "$DRYRUN_OUTPUT"
+    fi
+
     # Check if "Bisync successful" is in the dry-run output
     if ! grep -q "Bisync successful" "$DRYRUN_OUTPUT"; then
-	     # Log the error if dry-run did not contain "Bisync successful"
+        # Log the error if dry-run did not contain "Bisync successful"
         rm -f "$LOCKFILE"
-		touch "$ERROR_FILE"
         cat "$DRYRUN_OUTPUT" > "$ERROR_FILE"
         exit 1
-	fi
-	
-	# Run the actual bisync command
-	if $resync; then
-		rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync --max-lock 10 2>&1 | tee "$LOGFILE"
-	else
-		rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --max-lock 10 2>&1 | tee "$LOGFILE"
-		if grep -q "Must run --resync to recover" "$LOGFILE"; then
-			rm -f "$LOGFILE"
-			echo "RECOVERING WIT RESYNC FLAG"
-			rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync --max-lock 10 2>&1 | tee "$LOGFILE"
-		fi
-	fi
+    fi
 
-	if ! grep -q "Bisync successful" "$LOGFILE"; then
-		rm -f "$LOCKFILE"
-		touch "$ERROR_FILE"
-		cat "$LOGFILE" > "$ERROR_FILE"
-		exit 1
-	fi    
-	
-	rm -f "$LOCKFILE"
+    # Run the actual bisync command
+    if $resync; then
+        rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync --max-lock 10 2>&1 | tee "$LOGFILE"
+    else
+        rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --max-lock 10 2>&1 | tee "$LOGFILE"
+        if grep -q "Must run --resync to recover" "$LOGFILE"; then
+            rm -f "$LOGFILE"
+            echo "Recovering with --resync flag"
+            rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync --max-lock 10 2>&1 | tee "$LOGFILE"
+        fi
+    fi
+
+    if ! grep -q "Bisync successful" "$LOGFILE"; then
+        rm -f "$LOCKFILE"
+        cat "$LOGFILE" > "$ERROR_FILE"
+        exit 1
+    fi
+
+    rm -f "$LOCKFILE"
 fi
