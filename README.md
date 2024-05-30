49c1682ac252ca11fc8ca6acd8d593eee1aa551e
Erik-Jan van de Wal <erikfourofour@proton.me>
2024-05-29 23:09:50 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,61 @@
+# Rclone Sync
+
+## Overview
+
+This script offers a solution for bidirectional file synchronization between a local directory and a remote destination using Rclone. Using Rclone's experimental bisync functionality, it facilitates synchronization, ensuring data consistency between the local and remote locations.
+
+## Features
+
+- **Dry Run Mode**: The script automatically conducts a dryrun before synchronization to detect any potential issues.
+- **Resync Support**: Automatically handles recoverable errors by retrying with the `--resync` flag.
+- **Error Logging**: Logs errors encountered during synchronization for later inspection.
+
+## Usage
+
+### Prerequisites
+
+- [Rclone](https://rclone.org/) must be installed on your system.
+- [Zenity](https://help.gnome.org/users/zenity/stable/) must be installed on your system.
+### Running the Script
+
+1. Clone or download the script to your local machine.
+2. Make the script executable:
+    ```bash
+    chmod +x rclone_sync.sh
+    ```
+3. Run the script with the desired options:
+
+    ```bash
+    ./rclone_sync.sh --local "/path/to/local/directory" --remote "remote:directory"
+    ```
+
+### Command-line Options
+
+- `--local`: Specifies the local directory to sync with the remote destination.
+- `--remote`: Specifies the remote destination where files will be synced.
+
+### Example
+
+    ./rclone_sync.sh --local "/media/user/Proton/" --remote "proton:"
+
+### Automatic Synchronization with Cronjobs
+
+This script can be integrated with cronjobs to automate the synchronization process at regular intervals.
+
+## Error Handling and File Management
+
+- **Error Notification**: In case of synchronization errors, an error message will be displayed on the screen using Zenity.
+- **Error Log**: Additionally, an error log will be created at `/tmp/rclone_sync_error.log`. This log can be referenced for troubleshooting and analysis.
+- **Lock File**: To prevent the synchronization process from restarting prematurely, a lock file will be created at `/tmp/rclone_sync.lock`. This file ensures that only one instance of the synchronization script runs at a time, avoiding potential conflicts and data corruption.
+
+## Knowns issues
+
+- The syncing process is slow due to the precautionary dry-run before each actual sync to maintain data integrity. I'll explore methods to improve its speed.
+- Rclone's bisync feature encounters difficulty when both the local and remote paths contain no content. If all content is deleted and a sync operation is initiated, Rclone may encounter issues and attempt to resync all data. **Solution**: Ensure that there is always at least one file present in either the local or remote path to prevent synchronization issues with Rclone's bisync feature.
+
+
+
+
+# 
+
+PROTON PLEASE BUILD A LINUX TOOL FOR YOUR PROTON DRIVE KTHNX
---
+++ rclone_bisync.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Default values
+LOCKFILE="/tmp/rclone_bisync.lock"
+LOGFILE="/tmp/rclone_bisync.log"
+DRYRUN_OUTPUT="/tmp/rclone_bisync_dryrun.log"
+ERROR_FILE="/tmp/rclone_bisync_error.log"
+
+LOCAL_PATH=""
+REMOTE_PATH=""
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --local)
+            LOCAL_PATH="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        --remote)
+            REMOTE_PATH="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        *)  # unknown option
+            echo "Unknown option: $key"
+            exit 1
+            ;;
+    esac
+done
+
+# Check if LOCAL_PATH is empty
+if [ -z "$LOCAL_PATH" ]; then
+    echo "Error: --local argument is required."
+    exit 1
+fi
+
+# Check if REMOTE_PATH is empty
+if [ -z "$REMOTE_PATH" ]; then
+    echo "Error: --remote argument is required."
+    exit 1
+fi
+
+
+# Popup message using zenity
+show_popup() {
+    zenity --error --text="$1"
+}
+
+# Notification using notify-send
+send_notification() {
+    notify-send "rclone_bisync" "$1"
+}
+
+# Delete logs
+delete_logs() {
+	rm -f "$LOGFILE"
+	rm -f "$ERROR_FILE"
+	rm -f "$DRYRUN_OUTPUT"
+}
+
+# Check if the lock file exists and exit
+if [ -e "$LOCKFILE" ]; then
+    exit 1
+else
+	# Delete the logs from the previous sync
+	delete_logs
+    # Create the lock file
+    touch "$LOCKFILE"
+	# Boolean for resync
+	resync=false
+    # Run the rclone bisync command with dry-run and capture output
+    rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --dry-run 2>&1 | tee "$DRYRUN_OUTPUT"
+	# Rclone might fail with a recoverable error, this happens when there was never an initial sync, or when the paths have no content (possible rclone bug perhaps?)
+	if grep -q "Bisync aborted. Must run --resync to recover" "$DRYRUN_OUTPUT"; then
+		echo "RECOVERING WIT RESYNC FLAG"
+	    # Run the rclone bisync command with dry-run and --resync and capture the output
+		rm -f "$DRYRUN_OUTPUT"
+		resync=true
+    	rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync --dry-run 2>&1 | tee "$DRYRUN_OUTPUT"
+	fi
+	
+    # Check if "Bisync successful" is in the dry-run output
+    if ! grep -q "Bisync successful" "$DRYRUN_OUTPUT"; then
+	     # Log the error if dry-run did not contain "Bisync successful"
+		show_popup "Rclone (dry-run) bisync failed. See $ERROR_FILE for details."
+        touch "$ERROR_FILE"
+        cat "$DRYRUN_OUTPUT" > "$ERROR_FILE"
+        exit 1
+	fi
+	
+	# Run the actual bisync command
+	if $resync; then
+		rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force --resync 2>&1 | tee "$LOGFILE"
+	else
+		rclone bisync "$LOCAL_PATH" "$REMOTE_PATH" --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --drive-skip-gdocs --fix-case --force 2>&1 | tee "$LOGFILE"
+	fi
+
+	if ! grep -q "Bisync successful" "$LOGFILE"; then
+		touch "$ERROR_FILE"
+		cat "$LOGFILE" > "$ERROR_FILE"
+		show_popup "Rclone bisync failed. See $ERROR_FILE for details."
+		exit 1
+	fi    
+	
+	rm -f "$LOCKFILE"
+fi
